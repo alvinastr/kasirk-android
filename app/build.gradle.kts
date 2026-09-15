a8661090bd5709b37d2cced101ab767498e658d4
alvinastr <119154719+alvinastr@users.noreply.github.com>
2026-09-15 19:40:57 +0700

    chore: initialize KasirKita Android project

---
+++ app/build.gradle.kts
@@ -0,0 +1,133 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.kasirkita.pos"
+
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        applicationId = "com.kasirkita.pos"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Android Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    // =====================
+    // HILT
+    // =====================
+    implementation("com.google.dagger:hilt-android:2.59.2")
+    ksp("com.google.dagger:hilt-compiler:2.59.2")
+
+
+    // =====================
+    // RETROFIT
+    // =====================
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+
+    // =====================
+    // OKHTTP
+    // =====================
+    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
+
+
+    // =====================
+    // COROUTINES
+    // =====================
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
+    )
+
+
+    // =====================
+    // DATASTORE JWT
+    // =====================
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.7"
+    )
+
+
+    // =====================
+    // ROOM OFFLINE DATABASE
+    // =====================
+    implementation(
+        "androidx.room:room-runtime:2.7.2"
+    )
+
+    implementation(
+        "androidx.room:room-ktx:2.7.2"
+    )
+
+    ksp(
+        "androidx.room:room-compiler:2.7.2"
+    )
+
+
+    // =====================
+    // VIEWMODEL
+    // =====================
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2"
+    )
+
+
+    // =====================
+    // NAVIGATION
+    // =====================
+    implementation(
+        "androidx.navigation:navigation-compose:2.9.3"
+    )
+
+
+    // TEST
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/kasirkita/pos/KasirKitaApplication.kt
@@ -0,0 +1,7 @@
+package com.kasirkita.pos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KasirKitaApplication : Application()
